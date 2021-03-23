e260e12feb4d5fe07cba29b49d9f1321619c70b6
Davia DeNisco <davia622@gmail.com>
2021-03-23 13:20:18 -0400

    add randomcolor

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,28 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
+import randomColor from 'randomcolor';
 
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false)
   const [height, setHeight] = useState('initial')
+  const [hex, setHex] = useState('ffffff');
 
   const frontEl = useRef()
   const backEl = useRef()
+
+  const handleColorChange = () => {
+      setHex(randomColor());
+  }
 
   function setMaxHeight() {
     const frontHeight = frontEl.current.getBoundingClientRect().height
     const backHeight = backEl.current.getBoundingClientRect().height
     setHeight(Math.max(frontHeight, backHeight, 200))
-  }
+}
 
-  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
-  useEffect(() => {
+useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+useEffect(() => {
+      handleColorChange();
     window.addEventListener('resize', setMaxHeight)
     return () => window.removeEventListener('resize', setMaxHeight)
-  }, [])
+  }, []);
 
   return (
     <div
       className={`card ${flip ? 'flip' : ''}`}
-      style={{ height: height }}
+      style={{ height: height, border: `5px solid ${hex}`}}
       onClick={() => setFlip(!flip)}
     >
       <div className="front" ref={frontEl}>
@@ -35,7 +42,9 @@ export default function Flashcard({ flashcard }) {
         </div>
         : ''}
       </div>
-      <div className="back" ref={backEl}>{flashcard.answer}</div>
+      <div className="back" ref={backEl} style={{ backgroundColor: `${hex}50` }}>
+          {flashcard.answer}
+      </div>
     </div>
   )
 }
